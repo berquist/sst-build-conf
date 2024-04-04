c9158a3c0ac00deff4b5e26a9f2c57a52132c66e
Eric Berquist <ejberqu@sandia.gov>
2024-04-04 08:06:57 -0600

    memory simulators

---
+++ memory/dramsim3_mac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+dir_build=build
+dir_install="${PWD}"/install
+
+\rm -rf "${dir_build}" || true
+\rm -rf "${dir_install}" || true
+
+cmake \
+    -B"${dir_build}" \
+    -GNinja \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DCMAKE_INSTALL_PREFIX="${dir_install}" \
+    -DBUILD_SHARED_LIBS=1
+pushd "${dir_build}"
+ninja
+popd
