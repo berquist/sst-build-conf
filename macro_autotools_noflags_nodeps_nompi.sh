c2d583205da61e5e313fd894a94a3ef519293022
Eric Berquist <ejberqu@sandia.gov>
2024-05-31 13:08:05 -0600

    start on macro

---
+++ macro_autotools_noflags_nodeps_nompi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPTDIR}"/compilers.bash
+
+toolchain="${1}"
+
+source_compilers_nompi "${toolchain}"
+
+suffix=autotools_noflags_nodeps_nompi_${toolchain}
+
+dir_src="${PWD}/sst-macro"
+#dir_build="${PWD}"/sst-macro-build-${suffix}
+dir_build="${dir_src}"
+dir_install="${PWD}"/install_${suffix}
+
+#\rm -rf "${dir_build}" || true
+# \rm -rf "${dir_install}" || true
+
+pushd "${dir_src}"
+git clean -Xdf .
+./bootstrap.sh
+popd
+
+mkdir -p "${dir_build}"
+pushd "${dir_build}"
+
+INSTALL="$(command -v install) -p" "${dir_src}"/configure \
+       --prefix="${dir_install}"
+
+bear -- make install -j$(nproc)
+ln -fsv "${dir_build}"/compile_commands.json "${dir_src}"/compile_commands.json
