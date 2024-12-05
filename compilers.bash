79d94700db93c51c20bbfe196fc86ab40995f3ee
Eric Berquist <eric.berquist@gmail.com>
2024-12-05 18:19:51 -0500

    update common settings
    - do manual environment variable (error) handling
    - try setting PYENV_ROOT
    - try setting INTEL_PIN_DIRECTORY
    - set Python version for pyenv

--- compilers.bash
+++ compilers.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
+
+if [[ -z "${SPACK_COMPILER_SPEC}" ]]; then
+    echo "SPACK_COMPILER_SPEC not defined"
+    exit 1
+fi
+
+if ! command -v bear >&/dev/null; then
+    echo "bear not found in PATH"
+    exit 1
+fi
 
 source_compilers_nompi() {
     local toolchain="${1}"
@@ -30,8 +40,26 @@ source_compilers_mpi() {
 
     local ompi_version="4.1.5"
     local ompi_loc
+    # shellcheck disable=SC2086
     ompi_loc="$(spack location -i openmpi@${ompi_version} %${SPACK_COMPILER_SPEC})"
     export MPICC="${ompi_loc}"/bin/mpicc
     export MPICXX="${ompi_loc}"/bin/mpicxx
     export CPPFLAGS="-I${ompi_loc}/include"
 }
+
+if [[ -z "${PYENV_ROOT}" ]]; then
+    export PYENV_ROOT="${HOME}"/.pyenv
+fi
+
+# shellcheck disable=SC2034
+python_version=3.12.5
+
+# Handle the case where the Pin binary is on the path but the SST-specific
+# environment variable needed for the compile and link lines isn't present.
+if [[ -z "${INTEL_PIN_DIRECTORY}" ]]; then
+    pinloc="$(command -v pin)"
+    if [[ -n "${pinloc}" ]]; then
+        INTEL_PIN_DIRECTORY="$(dirname "$(dirname "${pinloc}")")"
+        export INTEL_PIN_DIRECTORY
+    fi
+fi
