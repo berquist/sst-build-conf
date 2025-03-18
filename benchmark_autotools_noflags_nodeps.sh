86acbbbafb9f8849f474dbdc904f08c2f6bd19ba
Eric Berquist <eric.berquist@gmail.com>
2025-03-18 11:16:45 -0400

    script for sst-benchmark

---
+++ benchmark_autotools_noflags_nodeps.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# shellcheck disable=SC2086
+# https://web.archive.org/web/20230401201759/https://wiki.bash-hackers.org/scripting/debuggingtips#making_xtrace_more_useful
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+set -x
+
+set -eo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${SCRIPTDIR}"/compilers.bash
+
+toolchain="${1}"
+
+source_compilers_mpi "${toolchain}"
+
+suffix=autotools_noflags_nodeps_${toolchain}
+
+dir_src="${PWD}"/sst-benchmark
+dir_build="${PWD}"/sst-benchmark-build-${suffix}
+dir_core="${PWD}"/install_${suffix}
+dir_install="${dir_core}"
+
+\rm -rf "${dir_build}" || true
+# \rm -rf "${dir_install}" || true
+
+pushd "${dir_src}"
+git clean -Xdf .
+./autogen.sh
+popd
+
+mkdir -p "${dir_build}"
+pushd "${dir_build}"
+
+INSTALL="$(command -v install) -p" "${dir_src}"/configure \
+       --prefix="${dir_install}" \
+       --with-sst-core="${dir_core}"
+
+"$(command -v bear)" -- make install -j"$(nproc)"
+ln -fsv "${dir_build}"/compile_commands.json "${dir_src}"/compile_commands.json
