b077ae6ecdce96c4bb634d36867d969c34808328
Eric Berquist <ejberqu@sandia.gov>
2026-05-28 08:41:38 -0600

    initial hgcc recipe

---
+++ buildscripts/hgcc_autotools_noflags_nodeps.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2086
+# https://web.archive.org/web/20230401201759/https://wiki.bash-hackers.org/scripting/debuggingtips#making_xtrace_more_useful
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+set -x
+
+set -eo pipefail
+
+SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${SCRIPTDIR}"/compilers.bash
+
+toolchain="${1}"
+
+source_compilers_mpi "${toolchain}"
+
+suffix="$(clean_suffix autotools_noflags_nodeps_${toolchain})"
+
+dir_src="${PWD}"/sst-hgcc
+dir_build="${PWD}"/sst-hgcc-build-${suffix}
+dir_core="${PWD}"/install_${suffix}
+dir_install="${dir_core}"
+
+\rm -rf "${dir_build}" || true
+# \rm -rf "${dir_install}" || true
+
+pushd "${dir_src}"
+git clean -Xdf .
+./autogen.sh
+popd
+
+mkdir -p "${dir_build}"
+pushd "${dir_build}"
+
+(
+    export PATH="${dir_install}/bin:${PATH}"
+    INSTALL="$(command -v install) -p" "${dir_src}"/configure \
+        --with-python="${python_config_loc}" \
+        --prefix="${dir_install}" \
+        --with-sst-elements="${dir_core}" \
+        --with-sst-core="${dir_core}"
+    bear_make_install
+)
+ln -fsv "${dir_build}"/compile_commands.json "${dir_src}"/compile_commands.json
